339990cc0b777fe4aada1ca9438340d5f27c0ae0
sheafitzek <agg817@sbcglobal.net>
2017-10-01 19:26:13 -0500

    setup root of app with routing

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
-// let's go!
+import React from 'react'; // importing a string (that is not a path) tells react to look in node_modules for the import
+import {render} from 'react-dom'; // an export inclosed in {} is a single export
+import {BrowserRouter, Match, Miss} from 'react-router';
+
+import './css/style.css'; // importing a path tells react to follow the file structure to find the import
+import App from './components/App'; // an export without {} is a default export
+
+import StorePicker from './components/StorePicker';
+import NotFound from './components/NotFound';
+
+const Root = ()=> {
+	return (
+		<BrowserRouter>
+			<div>
+				<Match exactly pattern="/" component={StorePicker}/>
+				<Match pattern="/store/:storeId" component={App}/>
+				<Miss component={NotFound}/>
+			</div>
+		</BrowserRouter>
+	)
+}
+
+render(<Root/>, document.querySelector(`#main`));
