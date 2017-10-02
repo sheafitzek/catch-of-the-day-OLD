8d7b84ffe7ea344a0bc4f18044c7e9cb31702f0b
sheafitzek <agg817@sbcglobal.net>
2017-10-01 19:42:38 -0500

    add App

---
+++ src/components/App.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+import Header from './Header';
+import Order from './Order';
+import Inventory from './Inventory';
+
+class App extends React.Component {
+	constructor() {
+		super();
+
+		this.addFish = this.addFish.bind(this);
+		// initial state
+		this.state = {
+			fishes: {},
+			order: {},
+		}
+	}
+
+	addFish(fish) {
+		const fishes = {...this.state.fishes};
+		const timestamp = Date.now();
+
+		fishes[`fish-${timestamp}`] = fish;
+		this.setState({fishes});
+	}
+
+	render() {
+		return (
+			<div className="catch-of-the-day">
+				<div className="menu">
+					<Header tagline="Fresh Seafood Market"/>
+				</div>
+				<Order/>
+				<Inventory addFish={this.addFish}/>
+			</div>
+		)
+	}
+}
+
+export default App;
