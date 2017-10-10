405fa818f328375ef7e927b4975c3affecaa7a6c
sheafitzek <agg817@sbcglobal.net>
2017-10-09 22:04:38 -0500

    render customer order

--- src/components/App.js
+++ src/components/App.js
@@ -49,11 +49,16 @@ class App extends React.Component {
 					<Header tagline="Fresh Seafood Market" />
 					<ul className="list-of-fishes">
 						{Object.keys(this.state.fishes).map((key) => (
-							<Fish key={key} index={key} details={this.state.fishes[key]} addToOrder={this.addToOrder} />
+							<Fish
+								key={key}
+								index={key}
+								details={this.state.fishes[key]}
+								addToOrder={this.addToOrder}
+							/>
 						))}
 					</ul>
 				</div>
-				<Order />
+				<Order fishes={this.state.fishes} order={this.state.order} />
 				<Inventory addFish={this.addFish} loadSamples={this.loadSamples} />
 			</div>
 		);
